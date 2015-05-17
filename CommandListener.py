66ea639e8b32fbd43d28c8a7ccc180836a1711da
Tyler Hall <tylerwhall@gmail.com>
2015-05-17 19:13:16 -0400

    commands work

---
+++ CommandListener.py
@@ -0,0 +1,69 @@
+from gi.repository import GLib, Gio
+import fcntl
+import os
+import re
+import logging
+
+class AsyncReader:
+    def __init__(self, f, callback):
+        self.f = f
+        self.callback = callback
+        self.buf = ""
+        fd = f.fileno()
+        fl = fcntl.fcntl(fd, fcntl.F_GETFL)
+        fcntl.fcntl(fd, fcntl. F_SETFL, fl | os.O_NONBLOCK)
+        channel = GLib.IOChannel.unix_new(f.fileno())
+        GLib.io_add_watch(channel, GLib.PRIORITY_DEFAULT, GLib.IO_IN, self.data_in, None)
+
+    def data_in(self, channel, condition, data):
+        self.buf += self.f.read()
+        while True:
+            match = re.match("(.*)[\n\r]+((?:.|[\n\r])*)", self.buf)
+            if not match:
+                break
+            line = match.group(1)
+            self.buf = match.group(2)
+            if len(line):
+                logging.debug("Got line: %s" % line)
+                self.callback(line)
+
+        return True
+
+COMMAND_MAP = {
+    "PLAY":     "play",
+    "MENABLE":  "play",
+    "MDISABLE": "pause",
+    "NEXT":     "next",
+    "PREVIOUS": "previous",
+}
+
+class CommandListener:
+    def __init__(self, f, controller):
+        self.controller = controller
+        self.reader = AsyncReader(f, self.handle_line)
+    
+    def handle_line(self, line):
+        try:
+            # Special case for LISTN command
+            if line.startswith("LIST"):
+                n = int(line[4])
+                self.controller.list(n)
+            else:
+                func = COMMAND_MAP[line]
+                getattr(self.controller, func)()
+        except Exception as e:
+            logging.debug("Unhandled command %s", line)
+
+if __name__ == "__main__":
+    import sys
+    class Controller:
+        def list(self, n):
+            print("LIST", n)
+
+        def play(self):
+            print("play")
+
+    logging.basicConfig(level=logging.DEBUG)
+    CommandListener(sys.stdin, Controller())
+    loop = GLib.MainLoop()
+    loop.run()
--- avrcp.py
+++ avrcp.py
@@ -1,19 +1,24 @@
-from dbus.mainloop.glib import DBusGMainLoop
 from gi.repository import GLib, Gio
 import logging
+import CommandListener
 
 MEDIA_PLAYER_IFC = 'org.bluez.MediaPlayer1'
 
 class AvrcpPlayers:
     def __init__(self, bus):
         self.bus = bus
-        paths = bus.call_sync('org.bluez', '/', 'org.freedesktop.DBus.ObjectManager', 'GetManagedObjects',
-                None, GLib.VariantType.new('(a{oa{sa{sv}}})'), 0, -1, None)
-        paths = paths.unpack()[0]
-        players = [path for path, interfaces in paths.items() if MEDIA_PLAYER_IFC in interfaces]
+        self.players = []
+        try:
+            paths = bus.call_sync('org.bluez', '/', 'org.freedesktop.DBus.ObjectManager', 'GetManagedObjects',
+                    None, GLib.VariantType.new('(a{oa{sa{sv}}})'), 0, -1, None)
+            paths = paths.unpack()[0]
+            players = [path for path, interfaces in paths.items() if MEDIA_PLAYER_IFC in interfaces]
+        except GLib.Error as e:
+            logging.error("Failed to get players %s" % e)
+            return
+
         logging.info("Found players %s" % players)
         flags = Gio.DBusProxyFlags.DO_NOT_LOAD_PROPERTIES
-        self.players = []
         for player in players:
             try:
                 p = Gio.DBusProxy.new_sync(bus, flags, None, 'org.bluez', player, MEDIA_PLAYER_IFC, None)
@@ -22,26 +27,51 @@ class AvrcpPlayers:
                 logging.error("Failed to create proxy for %s" % player)
                 continue
 
-    def play(self):
+    def call_safe(self, method):
         for player in self.players:
-            player.Play()
+            try:
+                getattr(player, method)()
+            except GLib.Error:
+                logging.error("Method %s failed for %s", method, player)
+
+    def play(self):
+        self.call_safe("Play");
 
     def pause(self):
-        for player in self.players:
-            player.Pause()
+        self.call_safe("Pause");
+
+    def next(self):
+        self.call_safe("Next");
+
+    def previous(self):
+        self.call_safe("Previous");
+
+class Controller:
+    def __init__(self, f_in):
+        self.bus = Gio.bus_get_sync(Gio.BusType.SYSTEM, None)
+        self.listener = CommandListener.CommandListener(f_in, self)
+
+    def list(self, n):
+        print("LIST", n)
+
+    def play(self):
+        AvrcpPlayers(self.bus).play()
+
+    def pause(self):
+        AvrcpPlayers(self.bus).pause()
+
+    def next(self):
+        AvrcpPlayers(self.bus).next()
+
+    def previous(self):
+        AvrcpPlayers(self.bus).previous()
 
 if __name__ == "__main__":
+    import sys
     logging.basicConfig(level=logging.DEBUG)
+
     loop = GLib.MainLoop()
 
-    bus = Gio.bus_get_sync(Gio.BusType.SYSTEM, None)
-
-    try:
-        players = AvrcpPlayers(bus)
-    except GLib.Error:
-        print("error")
-        raise
-
-    players.play()
+    Controller(sys.stdin)
 
     loop.run()
